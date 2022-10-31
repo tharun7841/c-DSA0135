089ba1627c8c7c6266714e728434137380166d1e
tharun7841 <112371398+tharun7841@users.noreply.github.com>
2022-10-31 14:56:05 +0530

    Add files via upload

---
+++ Area and perimeter of constructor.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+class Triangle
+{
+public:
+    int s1,s2,s3;
+    Triangle(int a,int b,int c)
+    {
+        s1 = a;
+        s2 = b;
+        s3 = c;
+    }
+    void print_area()
+    {
+        double s = (s1+s2+s3)/2.0;
+        cout << s << endl;
+        cout << "Perimeter is " << (s1+s2+s3) << endl;
+    }
+};
+int main()
+{
+    Triangle t(2,6,7);
+    t.print_area();
+    return 0;
+}
